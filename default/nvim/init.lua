ee383820b08cf87513eeee14ffc073836c29b9d3
Даниил Шило <crckhd10@gmail.com>
2022-03-24 23:05:22 +0300

    fix(restructure): Переструктурировал репозиторий

---
+++ default/nvim/init.lua
@@ -0,0 +1,12 @@
+--
+-- Main configuration file
+-- Authour: Daniil Shilo
+-- E-mail: crckhd10@gmail.com
+--
+
+require('main/settings')
+require('main/keybindings')
+
+require('packer_init')
+
+require('main/extras')
